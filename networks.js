5948fca7d44a6a30df465a56756fd2f7ecf5ff29
coinsolidation <coinsolidation@gmail.com>
2014-07-29 00:40:57 +0100

    add bitmark network

--- networks.js
+++ networks.js
@@ -7,39 +7,34 @@ var hex = function(hex) {
 exports.livenet = {
   name: 'livenet',
   magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
-  privKeyVersion: 128,
+  addressVersion: 85,
+  privKeyVersion: 73,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('c1fb746e87e89ae75bdec2ef0639a1f6786744639ce3d0ece1dcf979b79137cb'),
+    merkle_root: hex('d4715adf41222fae3d4bf41af30c675bc27228233d0f3cfd4ae0ae1d3e760ba8'),
     height: 0,
-    nonce: 2083236893,
+    nonce: 14385103,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    bits: 486604799,
+    timestamp: 1405274442,
+    bits: 0x1d00ffff,
   },
   dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
+    'seed.bitmark.co'
   ],
-  defaultClientPort: 8333
+  defaultClientPort: 9265
 };
 
 exports.mainnet = exports.livenet;
-
+// TODO add testnet
 exports.testnet = {
   name: 'testnet',
   magic: hex('0b110907'),
-  addressVersion: 0x6f,
-  privKeyVersion: 239,
+  addressVersion: 130,
+  privKeyVersion: 185,
   P2SHVersion: 196,
   hkeyPublicVersion: 0x043587cf,
   hkeyPrivateVersion: 0x04358394,
@@ -54,8 +49,7 @@ exports.testnet = {
     bits: 486604799,
   },
   dnsSeeds: [
-    'testnet-seed.bitcoin.petertodd.org',
-    'testnet-seed.bluematt.me'
+
   ],
   defaultClientPort: 18333
 };
