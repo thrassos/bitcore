1bb5dad19ba950e1340919f34c4ba1986037e99f
thrassos <tckalantzis@tips.com.gr>
2014-08-24 19:29:52 +0300

    changes for Solarcoin

--- networks.js
+++ networks.js
@@ -6,31 +6,32 @@ var hex = function(hex) {
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('fac3b6da'),
-  addressVersion: 1e,
+  magic: hex('04f104fd'),
+  addressVersion: 12,
   privKeyVersion: 9e,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('96841e6ecc8dc9643aaddfb6fcd616e08f0777c87b508f1c9fb35e461bea9774'),
-    merkle_root: hex('72ddd9496b004221ed0557358846d9248ecd4c440ebd28ed901efc18757d0fad'),
+    hash: hex('ee92f5a82a9acc1a9687e02412ec55e9051bd975a1d346f5e77f32fddb32cfed'),
+    merkle_root: hex('33ecdb1985425f576c65e2c85d7983edc6207038a2910fefaf86cfb4e53185a3'),
     height: 0,
-    nonce: 2447652,
+    nonce: 1397766,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1389388394,
+    timestamp: 1384473600,
     bits: 0x1e0ffff0,
   },
   dnsSeeds: [
     '127.0.0.1',
-	'74.208.230.160',
-	'216.250.125.121',
-	'195.130.216.149',
-	'96.18.212.86',
-	'188.226.239.21',
-	'54.201.183.106',
-	'213.81.142.62'
+	'173.246.19.219',
+	'173.77.211.102',
+	'192.83.142.151',
+	'122.249.91.18',
+	'178.23.132.73',
+	'213.91.205.134',
+	'71.92.43.217',
+	'108.0.230.225'
   ],
   defaultClientPort: 12024
 };
@@ -39,8 +40,8 @@ exports.mainnet = exports.livenet;
 // TODO add testnet
 exports.testnet = {
   name: 'testnet',
-  magic: hex('0b110907'),
-  addressVersion: 130,
+  magic: hex('fbc05af2'),
+  addressVersion: 92,
   privKeyVersion: 185,
   P2SHVersion: 196,
   hkeyPublicVersion: 0x043587cf,
